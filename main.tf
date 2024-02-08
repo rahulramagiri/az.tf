93fdfebe913986f7c5478ea5b77efdc8719fff0b
Balusp4 <159143519+Balusp4@users.noreply.github.com>
2024-02-08 14:00:32 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,70 +1,63 @@
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
 }
 
-resource "azurerm_resource_group" "resource-group" {
-  name = "terra-rg-demo"
-  location = "Central India"
+resource "azurerm_resource_group" "Azuretestbalu" {
+  name     = "Test98"
+  location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "demo-network" {
-    name = "tf-demo-network"
-    address_space =  ["10.0.0.0/16"]
-    location = azurerm_resource_group.resource-group.location
-    resource_group_name = azurerm_resource_group.resource-group.name
+
+resource "azurerm_virtual_network" "VNT" {
+  name                = "Yall"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.Azuretestbalu.location
+  resource_group_name = azurerm_resource_group.Azuretestbalu.name
 }
 
-resource "azurerm_subnet" "demo-subnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.resource-group.name
-  virtual_network_name = azurerm_virtual_network.demo-network.name
-  address_prefixes    = ["10.0.1.0/24"]
+resource "azurerm_subnet" "SNT" {
+  count                = 2
+  name                 = "internalsubnet-${count.index}"
+  resource_group_name  = azurerm_resource_group.Azuretestbalu.name
+  virtual_network_name = azurerm_virtual_network.VNT.name
+  address_prefixes     = ["10.0.${count.index}.0/24"]
 }
 
-resource "azurerm_network_interface" "demo-nic" {
-  name                = "my-nic"
-  location            = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
+resource "azurerm_network_interface" "NETIC" {
+  count                = 2  
+  name                = "example-nic-${count.index}"
+  location            = azurerm_resource_group.Azuretestbalu.location
+  resource_group_name = azurerm_resource_group.Azuretestbalu.name
 
   ip_configuration {
-    name                          = "myNICConfig"
-    subnet_id                     = azurerm_subnet.mySubnet.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SNT[count.index].id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
+resource "azurerm_windows_virtual_machine" "VMBalu" {
+  count                = 2  
+  name                = "TerraVM-${count.index}"
+  resource_group_name = azurerm_resource_group.Azuretestbalu.name
+  location            = azurerm_resource_group.Azuretestbalu.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.NETIC[count.index].id,
+  ]
 
-  vm_size = "Standard_DS1_v2"
+  os_disk {
+    name                 = "osdisk-${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    sku       = "2016-Datacenter"
     version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myOSDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  os_profile {
-    computer_name  = "myVM"
-    admin_username = "adminuser"
-    admin_password = "AdminPassword1234!"
-  }
-  
-  os_profile_windows_config {
-    enable_automatic_upgrades = true
-    provision_vm_agent       = true
   }
 }
